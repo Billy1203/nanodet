a25f8a2d4e9a97e30472f4a537bf5f9d9389b26c
Billy1203 <11510365@mail.sustech.edu.cn>
2021-03-31 16:15:02 +0800

    update shell on mac

--- demo_hand_webcam.sh
+++ demo_hand_webcam.sh
@@ -1,1 +1,1 @@
-python demo/demo.py webcam --config /home/yiheng/nanodet/config/train20210312.yml --model /home/yiheng/nanodet/demo/workspace/20210312_1/model_last.pth --camid 0
+python demo/demo.py webcam --config ./config/train20210312.yml --model ./demo/workspace/20210312_1/model_last.pth --camid 0
---
+++ demo_hand_webcam_transform.sh
@@ -0,0 +1,1 @@
+python demo/demo.py webcam --config ./config/Transformer/nanodet-t.yml --model ./demo/workspace/nanodet_t/model_last.pth --camid 0
