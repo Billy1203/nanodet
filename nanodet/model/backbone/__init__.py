c342ae23dc3bc69b1bfb631c983a57f0c1b7db41
Billy1203 <879697285@qq.com>
2021-04-08 20:29:06 +0800

    update

--- nanodet/model/backbone/__init__.py
+++ nanodet/model/backbone/__init__.py
@@ -6,9 +6,11 @@ from .mobilenetv2 import MobileNetV2
 from .efficientnet_lite import EfficientNetLite
 from .custom_csp import CustomCspNet
 from .repvgg import RepVGG
-# from .mobilenetv3 import MobileNetV3_Small
+from .mobilenetv3 import MobileNetV3_Small
 from .mobilenetv2_s import MobileNetV2S
 from .shufflenetv2_s import ShuffleNetV2S
+from .mobilenetv2_s2 import MobileNetV2S2
+from .mobilenetv3_2 import MobileNetV3
 
 
 def build_backbone(cfg):
@@ -32,8 +34,10 @@ def build_backbone(cfg):
         return MobileNetV2S(**backbone_cfg)
     elif name == 'ShuffleNetV2S':
         return ShuffleNetV2S(**backbone_cfg)
-    # elif name == 'MobileNetV3':
-    #     return MobileNetV3_Small(**backbone_cfg)
+    elif name == 'MobileNetV2S2':
+        return MobileNetV2S2(**backbone_cfg)
+    elif name == 'MobileNetV3':
+        return MobileNetV3_Small(**backbone_cfg)
     else:
         raise NotImplementedError
 
